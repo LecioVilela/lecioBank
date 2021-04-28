30bf56b4fb74c905db3e3bc696c5e35e403467a9
LecioVilela <lecio.vilela@yahoo.com>
2021-04-28 16:13:48 -0300

    upload project

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -2,6 +2,7 @@ class NegociacaoController {
     private _inputData: HTMLInputElement;
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
+    private _negociacoes = new Negociacoes();
 
     constructor() {
         this._inputData = <HTMLInputElement>document.querySelector('#data')
@@ -14,9 +15,17 @@ class NegociacaoController {
         const negociacao = new Negociacao(
             new Date(this._inputData.value.replace(/-/g, ',')),
             parseInt(this._inputQuantidade.value),
-            parseInt(this._inputValor.value)
+            parseFloat(this._inputValor.value)
         )
+            this._negociacoes.adiciona(negociacao)
 
-        console.log(negociacao)
+            this._negociacoes.paraArray().length = 0
+
+            this._negociacoes.paraArray().forEach(negociacao => {
+                console.log(negociacao.data)
+                console.log(negociacao.quantidade)
+                console.log(negociacao.valor)
+            })
+        
     }
 }
