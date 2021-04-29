637068ffb3845adb9b09db4917998bcb88420910
LecioVilela <lecio.vilela@yahoo.com>
2021-04-29 12:02:11 -0300

    upload project

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -3,11 +3,14 @@ class NegociacaoController {
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
     private _negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
+    private _mensagemView = new MensagemView('#mensagemView')
 
     constructor() {
         this._inputData = <HTMLInputElement>document.querySelector('#data')
         this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade')
         this._inputValor = <HTMLInputElement>document.querySelector('#valor')
+        this._negociacoesView.update(this._negociacoes);
     }
 
     adiciona(event: Event) {
@@ -19,13 +22,8 @@ class NegociacaoController {
         )
             this._negociacoes.adiciona(negociacao)
 
-            this._negociacoes.paraArray().length = 0
-
-            this._negociacoes.paraArray().forEach(negociacao => {
-                console.log(negociacao.data)
-                console.log(negociacao.quantidade)
-                console.log(negociacao.valor)
-            })
+            this._negociacoesView.update(this._negociacoes)
+            this._mensagemView.update('Negociação adicionada com sucesso!')
         
     }
 }
