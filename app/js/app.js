5215c72be78c1dfce7c832a2ac7ddde3f4572e71
LecioVilela <lecio.vilela@yahoo.com>
2021-04-29 16:38:47 -0300

    upload project

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,2 @@
 const controller = new NegociacaoController();
 $('form').submit(controller.adiciona.bind(controller));
-document.querySelector('.form')
-    .addEventListener('submit', controller.adiciona.bind(controller));
--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,6 +1,3 @@
 const controller = new NegociacaoController()
 
 $('form').submit(controller.adiciona.bind(controller))
-
-document.querySelector('.form')
-.addEventListener('submit', controller.adiciona.bind(controller))
